0292ead6911d3789de7ae1465fe2218595f51dcc
miroslawszyper <miroslawszyper@gmail.com>
2023-02-26 14:37:03 +0100

    add files

--- wsb_2023L/nst/php_5s6s_gr_2/1_db.php
+++ wsb_2023L/nst/php_5s6s_gr_2/1_db.php
@@ -1,3 +1,21 @@
 <?php 
-  echo "string";
+  // $conn = new mysqli("localhost", "root", "", "wsb_2023l_5s6s_sp2");
+  // echo $conn->connect_errno;
+  
+  require_once "./scripts/connect.php";
+  $sql = "SELECT * FROM `user`";
+  $result = $conn->query($sql);
+  //$user = $result->fetch_assoc();
+  //echo $user['firstName'];
+  $count = 0;
+  while ($user = $result->fetch_assoc()) {
+    // echo $user['firstName'];
+    $count++;
+    echo <<< USER
+      Użytkownik $count:<br>
+      Imię i nazwisko: $user[firstName] $user[lastName]<br>
+      Data urodzenia: $user[birthday]
+      <hr>
+USER;
+  }
  ?>
---
+++ wsb_2023L/nst/php_5s6s_gr_2/scripts/connect.php
@@ -0,0 +1,2 @@
+<?php 
+  $conn = new mysqli("localhost", "root", "", "wsb_2023l_5s6s_sp2");
